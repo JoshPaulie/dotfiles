12972a02b5be41a52435ed10d415a7b5a3471459
bex <30572280+JoshPaulie@users.noreply.github.com>
2024-07-25 07:17:44 -0500

    rm: colors

--- setup.sh
+++ setup.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
 
-# Color variables
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-NC='\033[0m'
-
-
-error_msg() {
-	echo "${RED}Error: $1${NC}"
-	exit 1
-}
-
-info_msg() {
-	echo "${BLUE}$1{$NC}: $2"
-}
-
 
 # Get to stowing!
-echo "${YELLOW}Setting up dotfiles..${NC}"
+echo "Setting up dotfiles.."
 for dir in */; do
 	if [ -d "$dir" ]; then
-		info_msg "Stowing" "$dir"
-		stow "$dir" || error_msg "failed to stow '${BLUE}$dir${NC}'"
+		echo "Stowing $dir"
+		stow "$dir" || echo "Error: Failed to stow $dir"
 	fi
 done
 
