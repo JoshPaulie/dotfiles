3b50c8fd329f7313fd6fafecb90893f94811cbff
bex <30572280+JoshPaulie@users.noreply.github.com>
2024-07-30 12:31:11 -0500

    add: more familiar line number settings; docs: add sections

--- neovim/dot-config/nvim/lua/config/vim.lua
+++ neovim/dot-config/nvim/lua/config/vim.lua
@@ -1,18 +1,26 @@
+-- Use system clipboard
 vim.opt.clipboard:append("unnamedplus")
 
-vim.wo.relativenumber = true
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
 
+-- Undo 
 vim.opt.undofile = true
 vim.opt.undodir = vim.fn.expand('~/.config/nvim-undo')
 
+-- Sane tabbing
 vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 
+-- Whitespace characters
 vim.opt.listchars = { space = '·', tab = '>·', trail = '~', extends = '>', precedes = '<', eol = ''}
 vim.api.nvim_set_keymap('n', '<leader>tl', ':set list!<CR>', { noremap = true, silent = true })
 
+-- Search highlighting
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
