06b3757bf3ab2bf20285b10d5dcda1a88c35c18f
bex <30572280+JoshPaulie@users.noreply.github.com>
2024-08-01 14:54:05 -0500

    add: incremental selection

--- neovim/dot-config/nvim/lua/plugins/treesitter.lua
+++ neovim/dot-config/nvim/lua/plugins/treesitter.lua
@@ -9,6 +9,13 @@ return {
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    node_incremental = "v",
+                    node_decremental = "V",
+                },
+            },
         })
     end
 }
