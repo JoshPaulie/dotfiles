feb7cc50e71c4b0a39d902db7e6a9f7323a7e3bf
bex <30572280+JoshPaulie@users.noreply.github.com>
2024-08-01 14:55:31 -0500

    tweak: trim ensured installed languages, make multi-line

--- neovim/dot-config/nvim/lua/plugins/treesitter.lua
+++ neovim/dot-config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,10 @@ return {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+            ensure_installed = {
+                "lua",
+                "python"
+            },
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
