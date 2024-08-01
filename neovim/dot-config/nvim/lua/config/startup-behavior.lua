b838cd58c68c8c3e57e0e0f2107d8be8b631f368
bex <30572280+JoshPaulie@users.noreply.github.com>
2024-08-01 09:50:52 -0500

    fix: startup function, more descriptive name

--- neovim/dot-config/nvim/lua/config/startup-behavior.lua
+++ neovim/dot-config/nvim/lua/config/startup-behavior.lua
@@ -1,13 +1,9 @@
--- This don't work, lol
-local function open_current_directory()
+local function open_cwd_if_no_args()
     local args = vim.fn.argv()
     if #args == 0 then
-        vim.cmd("cd .")
-        require('oil').open()
-        print("No arguments passed, starting Oil. (It didn't start.. did it?)")
+        vim.schedule(require("oil").open)
     end
 end
 
--- Attach function to startup event
-vim.api.nvim_create_autocmd("VimEnter", { callback = open_current_directory })
+vim.api.nvim_create_autocmd("VimEnter", { callback = open_cwd_if_no_args })
 
