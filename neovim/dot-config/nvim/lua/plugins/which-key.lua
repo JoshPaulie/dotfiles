9b09225f826c0739caa121281715c0e06a6ddad0
bex <30572280+JoshPaulie@users.noreply.github.com>
2024-08-04 15:09:47 -0500

    config: turn off icons

--- neovim/dot-config/nvim/lua/plugins/which-key.lua
+++ neovim/dot-config/nvim/lua/plugins/which-key.lua
@@ -1,7 +1,11 @@
 return {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+        icons = {
+            mappings = false
+        }
+    },
     keys = {
         {
             "<leader>?",
