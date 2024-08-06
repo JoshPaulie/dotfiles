f39015e2505ca55808f3141d4f8429825204519a
bex <30572280+JoshPaulie@users.noreply.github.com>
2024-08-06 13:18:37 -0500

    add: highlight-yank.lua

--- neovim/dot-config/nvim/init.lua
+++ neovim/dot-config/nvim/init.lua
@@ -2,5 +2,6 @@ require("config.lazy")
 require("config.vim")
 require("config.keymaps")
 require("config.startup-behavior")
+require("config.highlight-yank")
 
 vim.g.python3_host_prog = '/Users/bex/.config/nvim-venv/bin/python'
