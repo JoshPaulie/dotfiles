df988ba0fdf17ca73fadcbd5f3baf833227da6cb
bex <30572280+JoshPaulie@users.noreply.github.com>
2024-08-08 16:58:36 -0500

    add: treesitter textobjects

--- neovim/dot-config/nvim/lua/plugins/treesitter.lua
+++ neovim/dot-config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,13 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
     config = function()
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
+            -- Treesitter settings
             ensure_installed = {
                 "lua",
                 "python",
@@ -25,6 +27,29 @@ return {
                 keymaps = {
                     node_incremental = "v",
                     node_decremental = "V",
+                },
+            },
+
+            -- Textobjects settings
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                        ["al"] = "@statement.outer",
+                        ["aa"] = "@assignment.rhs",
+                    },
+                    selection_modes = {
+                        ['@parameter.outer'] = 'v', -- charwise
+                        ['@function.outer'] = 'V',  -- linewise
+                        ['@class.outer'] = '<c-v>', -- blockwise
+                    },
+                    include_surrounding_whitespace = false,
                 },
             },
         })
