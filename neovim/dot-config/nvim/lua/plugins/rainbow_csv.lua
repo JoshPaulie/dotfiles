c490f7f62e0bd0eb6e43a655d81cfd853b9f14f1
bex <30572280+JoshPaulie@users.noreply.github.com>
2024-08-09 12:40:27 -0500

    add: run :RainbowAlign when saving .csv files

--- neovim/dot-config/nvim/lua/plugins/rainbow_csv.lua
+++ neovim/dot-config/nvim/lua/plugins/rainbow_csv.lua
@@ -1,3 +1,14 @@
+local group = vim.api.nvim_create_augroup('csv_rainbow_align', { clear = true })
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+    group = group,
+    pattern = '*.csv',
+    callback = function()
+        vim.cmd('RainbowAlign')
+    end,
+    desc = 'Run :RainbowAlign on saving .csv files',
+})
+
 return {
     'cameron-wags/rainbow_csv.nvim',
     config = true,
